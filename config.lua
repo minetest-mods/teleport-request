924a16c06fc7631e74ef632d7e4ee9fabe29da8d
Panquesito7 <halfpacho@gmail.com>
2019-09-05 20:04:47 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 --[[
 Configuration
 
-Copyright (C) 2015-2019  Michael Tomaino (PlatinumArts@gmail.com)
+Copyright (C) 2015-2019 ChaosWormz
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
