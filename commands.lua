minetest-mods/teleport-request

commit cf6a55037873eae2717c9583edf6d64e538922d3

Update the timer in the formspec in real-time (#55)

commands.lua
@@ -69,7 +69,12 @@ minetest.register_chatcommand("tpn", {
 minetest.register_chatcommand("tpf", {
 	description = S("Show all teleport requests, made by you or to you, that are still active"),
 	privs = {interact = true, tp = true},
-	func = tp.list_requests
+	func = function(player)
+		local playername = minetest.get_player_by_name(player)
+
+		tp.tpf_update_time[playername] = true
+		tp.list_requests(playername)
+	end
 })
 
 minetest.register_chatcommand("tpr_mute", {

functions.lua
@@ -200,7 +200,9 @@ function tp.deny_request(id, own)
 	end
 end
 
-function tp.list_requests(playername)
+function tp.list_requests(player)
+	local playername = player:get_player_name()
+
 	local sent_requests = tp.get_requests(playername, "sender")
 	local received_requests = tp.get_requests(playername, "receiver")
 	local area_requests = tp.get_requests(playername, "area")
@@ -277,7 +279,23 @@ function tp.list_requests(playername)
 		formspec = ("size[8,%f]label[1,0.3;%s:]"):format(math.min(y,10),S("Teleport Requests"))
 			..request_list_formspec
 	end
+
 	minetest.show_formspec(playername, "teleport_request_list", formspec)
+
+	local function update_time()
+		if formspec == "" or string.find(formspec, S("You have no requests.")) then
+			tp.tpf_update_time[player] = false
+			return
+		end
+
+		if tp.tpf_update_time[player] then
+			-- TODO: find a way to edit the text only and update
+			-- the formspec without re-calling the function.
+			tp.list_requests(player)
+		end
+	end
+
+	minetest.after(1, update_time)
 end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
@@ -307,8 +325,12 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			changes = true
 		end
 	end
+
 	if changes and not fields.quit then
-		tp.list_requests(playername)
+		tp.tpf_update_time[player] = true
+		tp.list_requests(player)
+	elseif fields.quit then
+		tp.tpf_update_time[player] = false
 	end
 end)
 

init.lua
@@ -31,10 +31,11 @@ local S = minetest.get_translator(minetest.get_current_modname())
 
 tp = {
 	S = S,
-	tpr_list = {},
-	tphr_list = {},
-	tpc_list = {},
-	tpn_list = {}
+	tpr_list = { },
+	tphr_list = { },
+	tpc_list = { },
+	tpn_list = { },
+	tpf_update_time = { }
 }
 
 -- Clear requests when the player leaves

commit ee4954bdadeeb8d606d89f43a0e862588628ac7a

Fix call on nil player error in /tpf (#58)
Bug introduced in #55

commands.lua
@@ -70,10 +70,8 @@ minetest.register_chatcommand("tpf", {
 	description = S("Show all teleport requests, made by you or to you, that are still active"),
 	privs = {interact = true, tp = true},
 	func = function(player)
-		local playername = minetest.get_player_by_name(player)
-
-		tp.tpf_update_time[playername] = true
-		tp.list_requests(playername)
+		tp.tpf_update_time[player] = true
+		tp.list_requests(player)
 	end
 })
 

functions.lua
@@ -200,9 +200,7 @@ function tp.deny_request(id, own)
 	end
 end
 
-function tp.list_requests(player)
-	local playername = player:get_player_name()
-
+function tp.list_requests(playername)
 	local sent_requests = tp.get_requests(playername, "sender")
 	local received_requests = tp.get_requests(playername, "receiver")
 	local area_requests = tp.get_requests(playername, "area")
@@ -284,14 +282,14 @@ function tp.list_requests(player)
 
 	local function update_time()
 		if formspec == "" or string.find(formspec, S("You have no requests.")) then
-			tp.tpf_update_time[player] = false
+			tp.tpf_update_time[playername] = false
 			return
 		end
 
-		if tp.tpf_update_time[player] then
+		if tp.tpf_update_time[playername] then
 			-- TODO: find a way to edit the text only and update
 			-- the formspec without re-calling the function.
-			tp.list_requests(player)
+			tp.list_requests(playername)
 		end
 	end
 
@@ -327,13 +325,12 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	end
 
 	if changes and not fields.quit then
-		tp.tpf_update_time[player] = true
-		tp.list_requests(player)
+		tp.tpf_update_time[playername] = true
+		tp.list_requests(playername)
 	elseif fields.quit then
-		tp.tpf_update_time[player] = false
+		tp.tpf_update_time[playername] = false
 	end
 end)
-
 
 function tp.get_requests(playername, party)
 	local list
@@ -519,9 +516,7 @@ function tp.tpr_send(sender, receiver)
 				band = true
 			end
 		end, sender, receiver)
-
 	else
-
 	-- Compatibility with beerchat
 		if minetest.get_modpath("beerchat") and not minetest.check_player_privs(sender, {tp_admin = true}) then
 			if receiver == "" then
@@ -622,9 +617,7 @@ function tp.tphr_send(sender, receiver)
 				band = true
 			end
 		end, sender, receiver)
-
 	else
-
 	-- Compatibility with beerchat
 		if minetest.get_modpath("beerchat") and not minetest.check_player_privs(sender, {tp_admin = true}) then
 			if receiver == "" then
@@ -787,6 +780,7 @@ function tp.tpr_deny(name)
 	if (tp.count_requests(name, "sender") + tp.count_requests(name, "receiver")) > 1 then
 		-- Show formspec for decision
 		tp.list_requests(name)
+		tp.tpf_update_time[name] = true
 		return
 	end
 
@@ -814,6 +808,7 @@ function tp.tpr_accept(name)
 	if tp.count_requests(name, "receiver") > 1 then
 		-- Show formspec for decision
 		tp.list_requests(name)
+		tp.tpf_update_time[name] = true
 		return
 	end
 